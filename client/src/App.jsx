a6ada2dd6868359a13390915225ceb133940de6a
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-04-16 01:17:04 +0600

    error solb

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
-import Navbar from './Components/Navbar.jsx'
-import Home from './Components/Home.jsx'
-import './App.css'
-import Register from './Components/Register.jsx'
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Register from "./Components/Register";
+import { Routes, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./App.css";
 
 export default function App() {
   return (
-    <><Navbar />
-      <Home />
-      <Register/>
-    </>
-  )
+    <>
+        <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+     </>
+  );
 }
