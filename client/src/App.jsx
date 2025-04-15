6d4006be64a222d9b400078b8064d6f501c40fab
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-04-16 00:23:45 +0600

    Bootstrap add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 import Navbar from './Components/Navbar.jsx'
 import Home from './Components/Home.jsx'
 import './App.css'
+import Register from './Components/Register.jsx'
 
 export default function App() {
   return (
     <><Navbar />
-    <Home/>
+      <Home />
+      <Register/>
     </>
   )
 }
--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="mt-5">
       <div className="container">
-        <div className=" add_btn mt-5">
+        <div className=" add_btn mb-3">
           <button className="btn btn-primary">Add Data</button>
         </div>
         <table className="table">
@@ -19,6 +19,24 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Test</td>
+              <td>test@gmail.com</td>
+              <td>Developer</td>
+              <td>+121212</td>
+              <td className="d-flex justify-content-between">
+                <button className="btn btn-success">
+                  <i className="fa-solid fa-eye"></i>
+                </button>
+                <button className="btn btn-primary">
+                  <i className="fa-regular fa-pen-to-square"></i>
+                </button>
+                <button className="btn btn-danger">
+                  <i className="fa-solid fa-trash"></i>
+                </button>
+              </td>
+            </tr>
             <tr>
               <th scope="row">1</th>
               <td>Test</td>
